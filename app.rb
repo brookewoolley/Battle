48ee7251e8de98fe6dac5f23270fb645efb26445
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-24 16:04:35 +0000

    Set up attack function

--- app.rb
+++ app.rb
@@ -11,7 +11,13 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_one] = params[:player_one]
     session[:player_two] = params[:player_two]
+    session[:attack_history] = nil
     redirect "/play"
+  end
+
+  post '/attack' do
+    session[:attack_history] = 'attack'
+    redirect '/play'
   end
 
   get '/play' do
@@ -19,6 +25,7 @@ class Battle < Sinatra::Base
     @player_one_points = 0
     @player_two = session[:player_two]
     @player_two_points = 0
+    @attack_history = session[:attack_history]
     erb(:play)
   end
 
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -5,4 +5,10 @@ feature 'play' do
     sign_in_and_play
     expect(page).to have_content("Points")
   end
+
+  scenario " Players can attack" do
+    sign_in_and_play
+    click_button('Attack!')
+    expect(page).to have_content("Confirm attack!")
+  end
 end
